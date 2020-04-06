836a98c3d958e592f5c32bf4060ea28a1552a79a
AdRusinek <rusinekadrian19@gmail.com>
2020-04-06 22:47:33 +0200

    triggering service to brew

--- src/main/java/com/rusinek/msscbeerinventoryservice/config/JmsConfig.java
+++ src/main/java/com/rusinek/msscbeerinventoryservice/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package com.rusinek.msscbeerinventoryservice.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
