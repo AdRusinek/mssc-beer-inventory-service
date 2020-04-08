51831d34f5957302b8b4fdc6eebcf375cd0bd0b9
AdRusinek <rusinekadrian19@gmail.com>
2020-04-08 23:48:49 +0200

    adding allocation service

---
+++ src/main/java/com/rusinek/msscbeerinventoryservice/services/AllocationService.java
@@ -0,0 +1,11 @@
+package com.rusinek.msscbeerinventoryservice.services;
+
+import com.rusinek.brewery.model.BeerOrderDto;
+
+/**
+ * Created by Adrian Rusinek on 08.04.2020
+ **/
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
